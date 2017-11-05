325ff5e2863f0fe528b47bf8c61e53cf45dedd7e
AKA <nfeehan@gmail.com>
2017-11-05 12:11:50 -0500

    updating with latest uiboard firmware

--- display.ino
+++ display.ino
@@ -1,24 +1,32 @@
-void updateDisplay(long np){
+void updateDisplay(long np) {
   display.clearDisplay();
-  display.setTextSize(2);
+  display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(10,0);
-  display.println(np);
+  display.setCursor(0, 0);
   display.println(wavelabels[wavelabel]);
-  display.println(phaseOffset);
+  // hardcoded right now to display file number if on file mode
+  if (wavelabel == 4) {
+    display.setCursor(0, 10);
+    //  display.println(filename);
+    display.println("filename");
+  } else {
+    display.setCursor(0, 10);
+    display.println(ff);
+    display.println(" Hz");
+  }
   display.display();
 }
 
 
 void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
   uint8_t icons[NUMFLAKES][3];
- 
+
   // initialize
-  for (uint8_t f=0; f< NUMFLAKES; f++) {
+  for (uint8_t f = 0; f < NUMFLAKES; f++) {
     icons[f][XPOS] = random(display.width());
     icons[f][YPOS] = 0;
     icons[f][DELTAY] = random(5) + 1;
-    
+
     Serial.print("x: ");
     Serial.print(icons[f][XPOS], DEC);
     Serial.print(" y: ");
@@ -29,14 +37,14 @@ void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
 
   while (1) {
     // draw each icon
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
+    for (uint8_t f = 0; f < NUMFLAKES; f++) {
       display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, WHITE);
     }
     display.display();
     delay(200);
-    
+
     // then erase it + move it
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
+    for (uint8_t f = 0; f < NUMFLAKES; f++) {
       display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, BLACK);
       // move it
       icons[f][YPOS] += icons[f][DELTAY];
@@ -47,27 +55,27 @@ void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
         icons[f][DELTAY] = random(5) + 1;
       }
     }
-   }
+  }
 }
 
 void testdrawchar(void) {
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
 
-  for (uint8_t i=0; i < 168; i++) {
+  for (uint8_t i = 0; i < 168; i++) {
     if (i == '\n') continue;
     display.write(i);
     if ((i > 0) && (i % 21 == 0))
       display.println();
-  }    
+  }
   display.display();
   delay(1);
 }
 
 void testdrawcircle(void) {
-  for (int16_t i=0; i<display.height(); i+=2) {
-    display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
+  for (int16_t i = 0; i < display.height(); i += 2) {
+    display.drawCircle(display.width() / 2, display.height() / 2, i, WHITE);
     display.display();
     delay(1);
   }
@@ -75,9 +83,9 @@ void testdrawcircle(void) {
 
 void testfillrect(void) {
   uint8_t color = 1;
-  for (int16_t i=0; i<display.height()/2; i+=3) {
+  for (int16_t i = 0; i < display.height() / 2; i += 3) {
     // alternate colors
-    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color%2);
+    display.fillRect(i, i, display.width() - i * 2, display.height() - i * 2, color % 2);
     display.display();
     delay(1);
     color++;
@@ -85,10 +93,10 @@ void testfillrect(void) {
 }
 
 void testdrawtriangle(void) {
-  for (int16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
-    display.drawTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i, WHITE);
+  for (int16_t i = 0; i < min(display.width(), display.height()) / 2; i += 5) {
+    display.drawTriangle(display.width() / 2, display.height() / 2 - i,
+                         display.width() / 2 - i, display.height() / 2 + i,
+                         display.width() / 2 + i, display.height() / 2 + i, WHITE);
     display.display();
     delay(1);
   }
@@ -96,10 +104,10 @@ void testdrawtriangle(void) {
 
 void testfilltriangle(void) {
   uint8_t color = WHITE;
-  for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
-    display.fillTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i, WHITE);
+  for (int16_t i = min(display.width(), display.height()) / 2; i > 0; i -= 5) {
+    display.fillTriangle(display.width() / 2, display.height() / 2 - i,
+                         display.width() / 2 - i, display.height() / 2 + i,
+                         display.width() / 2 + i, display.height() / 2 + i, WHITE);
     if (color == WHITE) color = BLACK;
     else color = WHITE;
     display.display();
@@ -108,8 +116,8 @@ void testfilltriangle(void) {
 }
 
 void testdrawroundrect(void) {
-  for (int16_t i=0; i<display.height()/2-2; i+=2) {
-    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, WHITE);
+  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2) {
+    display.drawRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i, display.height() / 4, WHITE);
     display.display();
     delay(1);
   }
@@ -117,70 +125,70 @@ void testdrawroundrect(void) {
 
 void testfillroundrect(void) {
   uint8_t color = WHITE;
-  for (int16_t i=0; i<display.height()/2-2; i+=2) {
-    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, color);
+  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2) {
+    display.fillRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i, display.height() / 4, color);
     if (color == WHITE) color = BLACK;
     else color = WHITE;
     display.display();
     delay(1);
   }
 }
-   
+
 void testdrawrect(void) {
-  for (int16_t i=0; i<display.height()/2; i+=2) {
-    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, WHITE);
+  for (int16_t i = 0; i < display.height() / 2; i += 2) {
+    display.drawRect(i, i, display.width() - 2 * i, display.height() - 2 * i, WHITE);
     display.display();
     delay(1);
   }
 }
 
-void testdrawline() {  
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, 0, i, display.height()-1, WHITE);
+void testdrawline() {
+  for (int16_t i = 0; i < display.width(); i += 4) {
+    display.drawLine(0, 0, i, display.height() - 1, WHITE);
     display.display();
     delay(1);
   }
-  for (int16_t i=0; i<display.height(); i+=4) {
-    display.drawLine(0, 0, display.width()-1, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-  
-  display.clearDisplay();
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, display.height()-1, i, 0, WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-  
-  display.clearDisplay();
-  for (int16_t i=display.width()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
+  for (int16_t i = 0; i < display.height(); i += 4) {
+    display.drawLine(0, 0, display.width() - 1, i, WHITE);
     display.display();
     delay(1);
   }
   delay(250);
 
   display.clearDisplay();
-  for (int16_t i=0; i<display.height(); i+=4) {
-    display.drawLine(display.width()-1, 0, 0, i, WHITE);
+  for (int16_t i = 0; i < display.width(); i += 4) {
+    display.drawLine(0, display.height() - 1, i, 0, WHITE);
     display.display();
     delay(1);
   }
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE); 
+  for (int16_t i = display.height() - 1; i >= 0; i -= 4) {
+    display.drawLine(0, display.height() - 1, display.width() - 1, i, WHITE);
+    display.display();
+    delay(1);
+  }
+  delay(250);
+
+  display.clearDisplay();
+  for (int16_t i = display.width() - 1; i >= 0; i -= 4) {
+    display.drawLine(display.width() - 1, display.height() - 1, i, 0, WHITE);
+    display.display();
+    delay(1);
+  }
+  for (int16_t i = display.height() - 1; i >= 0; i -= 4) {
+    display.drawLine(display.width() - 1, display.height() - 1, 0, i, WHITE);
+    display.display();
+    delay(1);
+  }
+  delay(250);
+
+  display.clearDisplay();
+  for (int16_t i = 0; i < display.height(); i += 4) {
+    display.drawLine(display.width() - 1, 0, 0, i, WHITE);
+    display.display();
+    delay(1);
+  }
+  for (int16_t i = 0; i < display.width(); i += 4) {
+    display.drawLine(display.width() - 1, 0, i, display.height() - 1, WHITE);
     display.display();
     delay(1);
   }
@@ -190,12 +198,12 @@ void testdrawline() {
 void testscrolltext(void) {
   display.setTextSize(2);
   display.setTextColor(WHITE);
-  display.setCursor(10,0);
+  display.setCursor(10, 0);
   display.clearDisplay();
   display.println("scroll");
   display.display();
   delay(1);
- 
+
   display.startscrollright(0x00, 0x0F);
   delay(2000);
   display.stopscroll();
@@ -203,7 +211,7 @@ void testscrolltext(void) {
   display.startscrollleft(0x00, 0x0F);
   delay(2000);
   display.stopscroll();
-  delay(1000);    
+  delay(1000);
   display.startscrolldiagright(0x00, 0x07);
   delay(2000);
   display.startscrolldiagleft(0x00, 0x07);
--- weft-uiboard.ino
+++ weft-uiboard.ino
@@ -1,135 +1,139 @@
+#include <Wire.h> //Include arduino Wire Library to enable to I2C
 #include <Encoder.h>
-#include <SPI.h>
-#include <Wire.h>
+#include "Yurikleb_DRV2667.h"
+#include "waves.h"
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <elapsedMillis.h>
+#include <Audio.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+Yurikleb_DRV2667 drv;
 
-#define DISPLAY_PRESENT false
-
-#define sw 8 // encoder's pushbutton switch
-#define pwmOut 3 // PWM out for boost converter
-#define OLED_RESET 4 // not really conected, needed for adafruit oled lib
-Adafruit_SSD1306 display(OLED_RESET);
-
+#define OLED_RESET 20 // not really used
 #define NUMFLAKES 10
 #define XPOS 0
 #define YPOS 1
 #define DELTAY 2
 #define LOGO16_GLCD_HEIGHT 16
 #define LOGO16_GLCD_WIDTH  16
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000
-};
 
-#if (SSD1306_LCDHEIGHT != 64)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
+#define sw 8 // encoder's pushbutton switch
+Encoder myEnc(5, 6); // encoder A and B
 
-// Change these two numbers to the pins connected to your encoder.
-//   Best Performance: both pins have interrupt capability
-//   Good Performance: only the first pin has interrupt capability
-//   Low Performance:  neither pin has interrupt capability
-Encoder myEnc(6, 5);
-//   avoid using pins with LEDs attached
+float minFreq = 10;
+float maxFreq = 400;
 
-
-float minFreq = 0.0125 * 1000.0; // 0.0125 is too low for some people to feel, trying 0.016 now
-float maxFreq = 0.2 * 1000.0;
-float minDuty = 0.3;
-float maxDuty = 0.98;
-float dutyCycle = 0.95;
-float minAmpl = 0.0; // not tested
-float maxAmpl = 2000.0; // not tested
-float DACamplitude = 2000.0; // hardcoded to maximum
-
-float phase = 0.0;
-float twopi = 3.14159 * 2;
-float phaseOffset = 0.05;
+float ff = 0;
 volatile int wavelabelV = 0;
 int wavelabel = 0;
-int wavelabelOld = 1;
+int wavelabelOld = 0;
 int encUpLimit = 300;
 int encLoLimit = 0;
 long newPosition = 0;
 long oldPosition  = -999;
-char* wavelabels[] = {"SINE", "SQUARE", "SAW_DESC", "SAW_ASC", "NOISE"};
+char* wavelabels[] = {"SINE", "SQAR", "TRI", "NOIZ", "FILE"};
+char* filenames[] = {"SINE", "SQAR", "TRI", "NOIZ", "FILE"};
+elapsedMillis timeSincePlay;
+long waveformLength = 3000; //ms of waveform file to play, should eventually be dynamic
+boolean playingDigital = false;
+boolean analogSet = false;
+byte analogGain = 0x07; // can be from 0x04 to 0x07
+byte digitalGain = 0x03; // can be from 0x00 to 0x03
+
+// GUItool: begin automatically generated code
+AudioSynthNoiseWhite     theNoise;         //xy=135,300
+AudioSynthWaveform       theSine;      //xy=140,151
+AudioSynthWaveform       theSquare;      //xy=142,200
+AudioSynthWaveform       theTriangle;      //xy=142,248
+AudioMixer4              mixer1;         //xy=391,265
+AudioOutputAnalog        dac1;           //xy=675,246
+AudioConnection          patchCord1(theNoise, 0, mixer1, 3);
+AudioConnection          patchCord2(theSine, 0, mixer1, 0);
+AudioConnection          patchCord3(theSquare, 0, mixer1, 1);
+AudioConnection          patchCord4(theTriangle, 0, mixer1, 2);
+AudioConnection          patchCord5(mixer1, dac1);
+// GUItool: end automatically generated code
+
+
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00110000, B00110000,
+  B00110000, B00110000,
+  B00110000, B00110000,
+  B00110000, B00110000,
+  B00110000, B00110000,
+  B00110000, B00110000,
+  B00110000, B00110000,
+  B00110000, B00110000,
+  B00110000, B00110000,
+  B00110000, B00110000,
+  B00110000, B00110000,
+  B00110000, B00110000,
+  B00110000, B00110000,
+  B00110000, B00110000,
+  B00110000, B00110000,
+  B00110000, B00110000
+};
+#if (SSD1306_LCDHEIGHT != 32)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+Adafruit_SSD1306 display(OLED_RESET);
 
 void setup() {
-  pinMode(sw, INPUT_PULLUP);
-  pinMode(A14, OUTPUT);
+  // audio setup
+  AudioMemory(48);
+  mixer1.gain(0, 0.0); // sine
+  mixer1.gain(1, 0.9); // square
+  mixer1.gain(2, 0.0); // triangle
+  mixer1.gain(3, 0.0); // noise
+  theNoise.amplitude(0.8); //noise won't begin until a valid amp is specified
+  theSine.begin(WAVEFORM_SINE);
+  theSquare.begin(WAVEFORM_SQUARE);
+  theTriangle.begin(WAVEFORM_TRIANGLE);
+  theSine.begin(0.8, minFreq * 2, WAVEFORM_SINE);
+  theSquare.begin(0.8, minFreq * 2, WAVEFORM_SQUARE);
+  theTriangle.begin(0.8, minFreq * 2, WAVEFORM_TRIANGLE);
+  // ui setup
+  pinMode(sw, INPUT_PULLUP); // encoder pushbutton
+  pinMode(A14, OUTPUT); // DAC output
   attachInterrupt(sw, swTriggered, FALLING);
   analogWriteResolution(12);
-//  analogWriteFrequency(pwmOut, 375000*4);
-analogWriteFrequency(pwmOut, 375000);
-  pinMode(pwmOut, OUTPUT);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.display();
-  display.clearDisplay();
+  // i2c peripherals setup
+  drv.begin();
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.setRotation(3); // 2:rotates screen 180º
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  Serial.begin(9600);
+  Serial.println("hey now");
 }
 
 void loop() {
+
   readEnc();
   noInterrupts();
   wavelabel = wavelabelV;
   interrupts();
 
-  analogWrite(pwmOut, int(4096 * dutyCycle)); // duty cycle is a % and should have been dynamically calculated before here
-
-  // calculate and update the phase accumulator
-  phaseOffset = (4 * phaseOffset + constrain(map(newPosition, encLoLimit, encUpLimit, minFreq, maxFreq), minFreq, maxFreq)) / 5;
-  phase = phase + (phaseOffset / 1000.0);
-  if (phase >= twopi) {
-    phase = 0;
-  }
-  calculateFeedback(); // this doesn't exist yet
-  float DACval = 1;
-  switch (wavelabel) {
-    case 0: // SINE
-      DACval = sin(phase) * 2000.0 + 2050.0; // amplitude adjustment should occur here
-      break;
-    case 1: // SQUARE
-      // if phase > pi then 1 else 0
-      (phase > twopi / 2) ? (DACval = (DACamplitude / maxAmpl) * 4095.0) : (DACval = 0.0);
-      break;
-    case 2: // SAW_ASC
-      // phase itself is linearly ramping
-      DACval = floatmap(phase, 0, twopi, 1.0, 0.0) * (DACamplitude / maxAmpl) * 4095.0;
-      break;
-    case 3: // SAW_DESC
-      // phase itself is linearly ramping
-      DACval = floatmap(phase, 0, twopi, 0.0, 1.0) * (DACamplitude / maxAmpl) * 4095.0;
-      break;
-    case 4: // NOISE
-      // check this on the scope!
-      (random(0, 9) > 4.5) ? (DACval = (DACamplitude / maxAmpl) * 4095.0) : (DACval = 0.0);
-      break;
-    default: // SINE
-      DACval = sin(phase) * 2000.0 + 2050.0; // amplitude adjustment should occur here
-      break;
-  }
-
-  analogWrite(A14, (int)DACval);
   // only update display if something changed
   if ((newPosition != oldPosition) || (wavelabel != wavelabelOld) ) {
+    //    Serial.println("change detected " + wavelabel);
     oldPosition = newPosition;
     wavelabelOld = wavelabel;
     updateDisplay(newPosition);
+    updateFreq(newPosition);
+    switchTo(wavelabel);
   }
+
+  if (playingDigital && (timeSincePlay > waveformLength)) {
+    // retrigger the digital file if we are playing it
+    drv.playWaveGain(WaveForm_2, sizeof(WaveForm_2), digitalGain); //Play one of the Waveforms defined in waves.h;
+    timeSincePlay = 0;
+    playingDigital = true;
+    analogSet = false;
+  }
+
 }
 
 void swTriggered() {
@@ -138,7 +142,6 @@ void swTriggered() {
 }
 
 void readEnc() {
-  // read knob and
   newPosition = myEnc.read();
   if (newPosition > encUpLimit) {
     myEnc.write(encUpLimit);
@@ -147,3 +150,51 @@ void readEnc() {
     myEnc.write(encLoLimit);
   }
 }
+
+void updateFreq(int newPos) {
+  ff = constrain(floatmap(newPos, encLoLimit, encUpLimit, minFreq, maxFreq), minFreq, maxFreq);
+  //  Serial.println("changing freq to " + newPos);
+  theSine.frequency(ff);
+  theSquare.frequency(ff);
+  theTriangle.frequency(ff);
+}
+
+void switchTo(int wave) {
+  // mute all other waveforms, unmute waveform we want
+
+
+  if (wave == 4) {
+    Serial.print("file time");
+    mixer1.gain(0, 0.0);
+    mixer1.gain(1, 0.0);
+    mixer1.gain(2, 0.0);
+    mixer1.gain(3, 0.0);
+    analogSet = false;
+    if (!playingDigital) {
+      // play the digital file
+      drv.playWaveGain(WaveForm_2, sizeof(WaveForm_2), digitalGain); //Play one of the Waveforms defined in waves.h;
+      timeSincePlay = 0;
+      playingDigital = true;
+      analogSet = false;
+    } else {
+      if (timeSincePlay > waveformLength) {
+        playingDigital = false;
+      }
+    }
+  } else {
+    playingDigital = false;
+    if (!analogSet) {
+      drv.setToAnalogInputGain(analogGain);
+      analogSet = true;
+    }
+    for (int i = 0; i < 4; i++) {
+      if (i == wave) {
+        mixer1.gain(i, 0.9);
+      } else {
+        mixer1.gain(i, 0.0);
+      }
+    }
+
+  }
+}
+
